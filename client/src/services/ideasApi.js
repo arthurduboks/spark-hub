8b9d07043ce6add95bbd6b76e0d51e8244c57d4b
Arthur Duboks <duboks.arthur@gmail.com>
2023-11-13 15:08:24 -0500

    fixed API error

--- client/src/services/ideasApi.js
+++ client/src/services/ideasApi.js
@@ -1,17 +1,24 @@
 import axios from "axios";
+
 class IdeasApi {
   constructor() {
     this._apiUrl = "/api/ideas";
+
+    console.log(process.env.NODE_ENV);
   }
+
   getIdeas() {
     return axios.get(this._apiUrl);
   }
-  createNewIdea(data) {
+
+  createIdea(data) {
     return axios.post(this._apiUrl, data);
   }
+
   updateIdea(id, data) {
     return axios.put(`${this._apiUrl}/${id}`, data);
   }
+
   deleteIdea(id) {
     const username = localStorage.getItem("username")
       ? localStorage.getItem("username")
